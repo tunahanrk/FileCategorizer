627e7cd84de8be08b6bd6cc9cc41eabc27e7cedd
Tunahan ARIK <76400173+tunahanrk@users.noreply.github.com>
2023-05-17 14:01:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace FileCategorizer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Press Enter to categorize files in the current directory...");
+            Console.ReadLine();
+
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string[] files = Directory.GetFiles(currentDirectory);
+
+            foreach (string file in files)
+            {
+                string extension = Path.GetExtension(file);
+
+                string destinationFolder = Path.Combine(currentDirectory, extension.TrimStart('.'));
+                Directory.CreateDirectory(destinationFolder);
+
+                string destinationFile = Path.Combine(destinationFolder, Path.GetFileName(file));
+                File.Move(file, destinationFile);
+            }
+
+            Console.WriteLine("Files categorized and moved successfully.");
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
